a948149cee97df2cf701cd90b7329fcaab9d8e07
jGean09 <josegeantlc@gmail.com>
2024-09-03 13:20:17 -0300

    alterações

--- cars/views.py
+++ cars/views.py
@@ -2,8 +2,18 @@ from django.shortcuts import render
 from cars.models import car
 
 def cars_view(request):
-    cars = car.objects.all()
-    print(cars)
+    cars = car.objects.all() 
+    search = request.GET.get('search')
+    #print(request)
+    if search:
+        # icontains vs contains(do mesmo jeito da string)
+        cars = car.objects.filter(model__icontains=search).order_by('-model')
+    
+    
+    #cars = car.objects.filter(brand__name = "fiat")
+    #cars = car.objects.filter(model__contains = "Mustang")
+    #"model__contains" qualquer string que contém essa string "Fusca"
+    #print(cars)
     
     return render(
         request,
