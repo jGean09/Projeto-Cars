8fe96962876a2e3f047d08769da37a57a92c288b
jGean09 <josegeantlc@gmail.com>
2024-09-03 15:02:22 -0300

    git

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from cars.views import cars_view
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cars/', cars_view, name='cars_list'),
-] + static(settings.MEDIA_URL,documento_root= settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cars/views.py
+++ cars/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render
 from cars.models import car
 
 def cars_view(request):
-    cars = car.objects.all() 
+    cars = car.objects.all()
     search = request.GET.get('search')
     #print(request)
     if search:
         # icontains vs contains(do mesmo jeito da string)
-        cars = car.objects.filter(model__icontains=search).order_by('-model')
-    
+        cars = car.objects.filter(model__icontains=search).order_by('model')
+    else:
+        cars = car.objects.all().order_by('-model')
     
     #cars = car.objects.filter(brand__name = "fiat")
     #cars = car.objects.filter(model__contains = "Mustang")
