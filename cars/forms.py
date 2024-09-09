525a10ba33ba3723d2e5a72631dc80d063949433
jGean09 <josegeantlc@gmail.com>
2024-09-09 20:53:11 -0300

    Criando tela de super usuario

--- cars/forms.py
+++ cars/forms.py
@@ -33,3 +33,21 @@ class CarModelForm(forms.ModelForm):
 
 
 
+    def clean_value(self):
+        value = self.cleaned_data.get('value')
+        if value < 20000:
+            self.add_error('value', 'valor mínimo do carro deve ser de R$ 20.000')
+        return value
+    
+    def clean_factory_year(self):
+        factory_year = self.cleaned_data.get('factory_year')
+        if factory_year < 1975:
+            self.add_error('factory_year', 'O carro não é possível cadastrar carro fabricados antes de 1975')
+        return factory_year
+    def clean_photo(self):
+        photo = self.cleaned_data.get('photo')
+        if not photo:
+            self.add_error('photo','Não pode cadastrar carro sem foto')
+        return photo
+   
+    
